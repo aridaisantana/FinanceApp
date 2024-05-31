dfceb1854d01589fea312bd14b83022bac28a437
Aridai Santana <aridaitecnologia@gmail.com>
2024-05-31 17:37:21 +0100

    Addition of Stock Controller, ApplicationDBContext and Database Migrations

---
+++ FinanceAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using FinanceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
