c9cdd5189545b6da9236eaa774200d3d406969ce
Aridai Santana <aridaitecnologia@gmail.com>
2024-06-02 19:06:41 +0100

    Pagination for stocks getAll method added

--- FinanceAPI/Repository/StockRepository.cs
+++ FinanceAPI/Repository/StockRepository.cs
@@ -58,7 +58,10 @@ namespace FinanceAPI.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
